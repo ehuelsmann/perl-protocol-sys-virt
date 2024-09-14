3e3d3c24007b80d16bd897edd077d9d3d84e316c
Erik Huelsmann <ehuels@gmail.com>
2024-09-14 11:56:05 +0200

    Regenerate with XDR::Gen v0.0.3

--- lib/Protocol/Sys/Virt.pm
+++ lib/Protocol/Sys/Virt.pm
@@ -2,7 +2,7 @@
 use v5.14;
 use warnings;
 
-package Protocol::Sys::Virt v10.3.4;
+package Protocol::Sys::Virt v10.3.5;
 
 1;
 
@@ -12,7 +12,7 @@ Protocol::Sys::Virt - Abstract LibVirt protocol implementation
 
 =head1 VERSION
 
-v10.3.4
+v10.3.5
 
 Based on LibVirt tag v10.3.0
 
