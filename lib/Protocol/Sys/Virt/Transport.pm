1dbe14fe2f2ef0ae2bbe4a7c93398c1ff1dc7abc
Erik Huelsmann <ehuels@gmail.com>
2024-09-22 21:44:58 +0200

    Increase version number

--- lib/Protocol/Sys/Virt/Transport.pm
+++ lib/Protocol/Sys/Virt/Transport.pm
@@ -13,7 +13,7 @@
 use v5.14;
 use warnings;
 
-package Protocol::Sys::Virt::Transport v10.3.7;
+package Protocol::Sys::Virt::Transport v10.3.8;
 
 use Carp qw(croak);
 use Log::Any qw($log);
@@ -334,7 +334,7 @@ Protocol::Sys::Virt::Transport - Low level Libvirt connection protocol
 
 =head1 VERSION
 
-v10.3.7
+v10.3.8
 
 Based on LibVirt tag v10.3.0
 
