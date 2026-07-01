d86d8d07467aaa4a1e5f973672b4a8a2794d9afe
Erik Huelsmann <ehuels@gmail.com>
2026-07-01 21:21:37 +0200

    Release v12.5.1

--- lib/Protocol/Sys/Virt/Remote.pm
+++ lib/Protocol/Sys/Virt/Remote.pm
@@ -13,13 +13,13 @@
 use v5.14;
 use warnings;
 
-package Protocol::Sys::Virt::Remote v12.5.0;
+package Protocol::Sys::Virt::Remote v12.5.1;
 
 use Carp qw(croak);
 use Log::Any qw($log);
 
-use Protocol::Sys::Virt::Remote::XDR v12.5.0;
-use Protocol::Sys::Virt::Transport::XDR v12.5.0;
+use Protocol::Sys::Virt::Remote::XDR v12.5.1;
+use Protocol::Sys::Virt::Transport::XDR v12.5.1;
 my $remote = 'Protocol::Sys::Virt::Remote::XDR';
 my $transport = 'Protocol::Sys::Virt::Transport::XDR';
 
@@ -3072,7 +3072,7 @@ Protocol::Sys::Virt::Remote - Connect to remote libvirt daemon
 
 =head1 VERSION
 
-v12.5.0
+v12.5.1
 
 Based on LibVirt tag v12.5.0
 
